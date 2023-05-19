c4a2125e0a151dcde4ff780a7ef9b2276829f916
Musta <polettiignacio7@gmail.com>
2023-05-19 12:58:36 -0300

    [feature] added data and themes

---
+++ src/constants/data/categories.js
@@ -0,0 +1,41 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+
+    name: "PC armadas",
+
+    color: "#8FBFE0",
+  },
+
+  {
+    id: 2,
+
+    name: "Hardware",
+
+    color: "#7C77B9",
+  },
+
+  {
+    id: 3,
+
+    name: "Notebook",
+
+    color: "#1D8A99",
+  },
+
+  {
+    id: 4,
+
+    name: "Placas de video",
+
+    color: "#0BC9CD",
+  },
+
+  {
+    id: 5,
+
+    name: "Monitores",
+
+    color: "#14FFF7",
+  },
+];
---
+++ src/constants/themes/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  primary: "#5611b3",
+  secondary: "#09072c",
+  terciary: "#35ffd2",
+  background: "#eeeeee",
+  black: "#00000",
+  white: "ffffff",
+};
